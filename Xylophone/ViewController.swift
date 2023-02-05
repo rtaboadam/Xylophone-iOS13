b9f19e7ebf74ed94d0765b2d2e7e4812a928d4fb
Ricardo Taboada <tabo.odricar@gmail.com>
2023-02-04 22:02:09 -0600

    [Added] Naive way to implement sounds in iOS.
    On the app brewery course; Angela, the instructor, aims the students to obtain help from StackOverflow.
    This help us to add a way to put sounds in our app
    - https://stackoverflow.com/questions/32036146/how-to-play-a-sound-using-swift
    
    Also help us to understand the Apple docs
    
    - https://developer.apple.com/documentation/avfaudio/avaudiosession/category/1616509-playback

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: Any) {
+        playSound()
+    }
     
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            guard let player = player else { return }
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
