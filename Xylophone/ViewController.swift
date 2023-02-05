f8d4ffbbe5651b0c7d564dd3413e53091c6b2b63
Ricardo Taboada <tabo.odricar@gmail.com>
2023-02-04 23:11:45 -0600

    [Complete] Boss Challenge.
    The challenge was complete by looking the `setInterval` on Swift. This is possible by calling `Time.schedulerTimer`
    After having the setInterval we only look the `alpha` property to modify it on the function
    
    - https://www.hackingwithswift.com/articles/117/the-ultimate-guide-to-timer

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -9,33 +9,27 @@
 import UIKit
 import AVFoundation
 
-
 class ViewController: UIViewController {
-    var player: AVAudioPlayer?
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func keyPressed(_ sender: Any) {
-        playSound()
-    }
-    
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
-        
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            guard let player = player else { return }
-            player.play()
-        } catch let error {
-            print(error.localizedDescription)
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.2
+        playSound(sender.currentTitle!)
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { _ in
+            sender.alpha = 1
         }
     }
-
+    
+    func playSound(_ note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
+
 
